e072cc42ae149c2b39f4fc185635096690cda632
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-29 11:06:45 +0200

    ...

--- parse_csv.py
+++ parse_csv.py
@@ -1,8 +1,7 @@
 import csv
 # context manager - file closes automatically
 with open("data.csv") as my_file:
-  
-  # Can you show the name, last_name, note
-  # A, Aa, xxxxxxxx
-
+  file_reader = csv.reader(my_file, delimiter=",", quotechar='"')
+  for row in file_reader:
+    print(row[0], row[1], row[2])
 
