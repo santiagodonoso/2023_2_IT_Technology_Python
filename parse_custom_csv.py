42b46a467b02b649dc572f02dee1d32908122c43
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-29 12:26:39 +0200

    ...

--- parse_custom_csv.py
+++ parse_custom_csv.py
@@ -1,7 +1,7 @@
 import csv
 # context manager - file closes automatically
-with open("data.csv", encoding="utf-8") as my_file:
-  file_reader = csv.reader(my_file, delimiter=",", quotechar='"')
+with open("custom.csv", encoding="utf-8") as my_file:
+  file_reader = csv.reader(my_file, delimiter="¤", quotechar='"')
   next(file_reader) # Go to next row
   print("Users\n")
   for row in file_reader:
