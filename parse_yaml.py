025362de014438e96f9243444eadff78de0a97c4
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-29 14:17:37 +0200

    ...

--- parse_yaml.py
+++ parse_yaml.py
@@ -5,5 +5,7 @@ with open("data.yaml", "r") as yaml_file:
 print(data["name"])
 print(data["last_name"])
 
-# Loop trhought siblings and show their name
-
+# Loop through siblings and show their name
+for sibling in data["siblings"]:
+  print(sibling.get("name"))
+  print(sibling["name"])
