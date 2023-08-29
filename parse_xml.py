2e8b8c8629716e70fc18ff59dac8ec1abca61413
Santiago Donoso <santiagodonoso@gmail.com>
2023-08-29 10:08:16 +0200

    ...

--- parse_xml.py
+++ parse_xml.py
@@ -1,4 +1,14 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse('data.xml')
-print(tree)
+root = tree.getroot() # person
+# print(root[0].text)
+name = tree.findtext('name').strip()
+last_name = tree.findtext('last_name').strip()
+print(f"Hi {name} {last_name}")
+ 
+#Today     is     ok
+# Hi A B
+# f string
+
+
 
