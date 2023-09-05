fd7620afb1ac304e2f65333569d191fee05dce8d
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-05 12:59:51 +0200

    ...

--- arango_get_user.py
+++ arango_get_user.py
@@ -1,11 +1,18 @@
 import requests
+import json
 
 res = requests.get("http://127.0.0.1:8529/_api/document/users/128943", 
 auth=("root", "password"))
 
-print(res)
+# print(res) # <Response [200]>
+# print(dir(res))
 
+# print(type(res.content)) # <class 'bytes'>
+# Convert the res to JSON
+res = json.loads(res.content) # Convert res to JSON
 
+# Show this in the teminal:
+# Hi A Aa, your age is 1
 
 
 
