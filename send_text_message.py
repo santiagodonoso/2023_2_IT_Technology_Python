207e8c79609d5ad3cdd2c3e6d575a4cca08015b3
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-12 10:41:01 +0200

    ...

--- send_text_message.py
+++ send_text_message.py
@@ -2,11 +2,11 @@ import requests
 
 url = "https://fiotext.com/send-sms"
 myobj = {
-  "user_api_key": "e13d1592688a13ae9aa0df5bd5fe554f",
+  "user_api_key": "YOUR_API_KEY_HERE",
   "sms_to_phone" : "YOUR_PHONE_NUMBER_HERE",
   "sms_message": "MESSAGE HERE"
   }
 
-x = requests.post(url, data = myobj)
+x = requests.post(url, data = myobj) # data is "body form-data"
 
 print(x.text)
