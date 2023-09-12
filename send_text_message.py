42f3aa1f620c11c7378a18eed3379c691770b71e
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-12 10:47:20 +0200

    ...

--- send_text_message.py
+++ send_text_message.py
@@ -1,6 +1,11 @@
 import requests
 
 url = "https://fiotext.com/send-sms"
+
+# Do you remember context managers?
+# Get the phone number from the file "user_data.json"
+# Extract the number and replace it with "YOUR_PHONE_NUMBER_HERE"
+# Note: The file is text, you must convert it to JSON - loads()
 myobj = {
   "user_api_key": "YOUR_API_KEY_HERE",
   "sms_to_phone" : "YOUR_PHONE_NUMBER_HERE",
