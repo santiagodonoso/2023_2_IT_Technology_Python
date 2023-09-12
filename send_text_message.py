50dcb8e1e87c3927fdd42750e0653cd87a3e2171
Santiago Donoso <santiagodonoso@gmail.com>
2023-09-12 11:14:04 +0200

    ...

--- send_text_message.py
+++ send_text_message.py
@@ -1,6 +1,16 @@
 import requests
+import json
 
 url = "https://fiotext.com/send-sms"
+
+with open("user_data.json", "r", encoding="utf-8") as my_file:
+  # print(dir(my_file))
+  # print(my_file.read())
+  json_data = json.loads(my_file.read())
+  print(type(json_data))
+  mobile_number = json_data.get("user_mobile_number", "DEFAULT_NUMBER_HERE")
+
+print(mobile_number)
 
 # Do you remember context managers?
 # Get the phone number from the file "user_data.json"
@@ -8,10 +18,8 @@ url = "https://fiotext.com/send-sms"
 # Note: The file is text, you must convert it to JSON - loads()
 myobj = {
   "user_api_key": "YOUR_API_KEY_HERE",
-  "sms_to_phone" : "YOUR_PHONE_NUMBER_HERE",
+  "sms_to_phone" : mobile_number,
   "sms_message": "MESSAGE HERE"
   }
-
 x = requests.post(url, data = myobj) # data is "body form-data"
-
 print(x.text)
